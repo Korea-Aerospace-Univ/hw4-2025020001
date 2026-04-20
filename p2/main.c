2ca70cd2254b0c5a6bfe534324b9c4ee2bff7248
2025020001 <kkyc060707@gmail.com>
2026-04-20 16:06:58 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=0, k=0, count1=0, max1=0, count2=0, max2=0;
+    char j='\n';
+    scanf("%d", &i);
+    getchar();
+    
+    do{
+        scanf("%c",&j);
+        if(j>='a' && j<='z') {
+            count1 ++;
+            count2=0;
+            if (count1>max1)
+                max1 = count1;}
+            
+        else if(j>='0'&& j<='9'){
+            count2 ++;
+            count1=0;
+            if (count2 > max2)
+                max2 = count2;}
+        else{
+            count1=0;
+            count2=0;
+        }    
+        k ++;
+    }while(k<i);
+    
+    printf("%d\n", max1);
+    printf("%d", max2);
+    return 0;
+}
